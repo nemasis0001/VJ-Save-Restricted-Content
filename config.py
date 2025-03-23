29bb66e14ae2ee73ce8cc15b84dff4351e5b308c
nemasis0001 <nemasis34678@gmail.com>
2025-03-23 20:26:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,14 +9,17 @@ API_ID = int(os.environ.get("API_ID", "28348410"))
 # Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "f6db276c339d8c900ecb2fb588128ede")
 
-
-# Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "6073523936"))
+# Your Owner / Admin Id For Broadcast (Supports Multiple Admins)
+ADMINS = list(map(int, os.environ.get("ADMINS", "6073523936").split()))
 
 # Your Mongodb Database Url
-# Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = os.environ.get(
     "DB_URI", 
-    "mongodb+srv://adityanegi735:your_real_password@cluster0.ig6gv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
-ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
+    "mongodb+srv://adityanegi735:your_real_password@cluster0.ig6gv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
+
+# Your Database Name
+DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
+
+# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then False
+ERROR_MESSAGE = os.environ.get('ERROR_MESSAGE', 'True').lower() == 'true'
